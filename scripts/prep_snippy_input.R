e9da02c7dc3842baa2c26998f1e6151ad7f5cfe1
Tamas Stirling <stirling.tamas@gmail.com>
2022-03-06 13:34:17 +0100

    Fix typo

--- scripts/prep_snippy_input.R
+++ scripts/prep_snippy_input.R
@@ -1,11 +1,12 @@
 rm(list=ls())
-con <- file(paste0(
-  "./jobs/", jobname, "/output/","log_", format(Sys.time(), "%Y%m%d"), ".txt"))
-sink(con, append = TRUE, split = TRUE)
 
 args <- commandArgs(trailingOnly = TRUE)
 jobname <- args[1]
 source_dir <- args[2]
+
+con <- file(paste0(
+  "./jobs/", jobname, "/output/","log_", format(Sys.time(), "%Y%m%d"), ".txt"))
+sink(con, append = TRUE, split = TRUE)
 
 output_dir <- paste0("./jobs/", jobname, "/output/")
 if (!dir.exists(output_dir)) dir.create(output_dir, recursive = TRUE)
