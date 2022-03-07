e53181860905f94bbf40f7c2fbfd803f8b72c8f6
Tamas Stirling <stirling.tamas@gmail.com>
2022-03-07 08:28:03 +0100

    Merge genomes from all three snippy directories

--- scripts/merge_genomes.R
+++ scripts/merge_genomes.R
@@ -3,22 +3,26 @@ rm(list=ls())
 args <- commandArgs(trailingOnly = TRUE)
 jobname <- args[1]
 
-output_dir <- paste0("./jobs/", jobname, "/output/snippy/")
-genomenames <- list.dirs(output_dir, full.names= FALSE, recursive = FALSE)
+output_dir <- paste0("./jobs/", jobname, "/output/snippy")
 
 genomes <- list()
-for (i in genomenames) {
-  f <- seqinr::read.fasta(
-    file = paste0(output_dir, i, "/snps.consensus.subs.fa"),
-    forceDNAtolower = FALSE)
-  newname <- strsplit(i, "_")[[1]][1]
-  
-  #only keep chromosome - add later as decision point??
-  genomes[[newname]] <- f[[1]]
+d1 <- list.dirs(output_dir, recursive = FALSE)
+for (i in d1) {
+  d2 <- list.dirs(i, recursive = FALSE)
+  if (length(d2) > 0) {
+    for (j in d2) {
+      f <- seqinr::read.fasta(
+        file = paste0(j, "/snps.consensus.subs.fa"),
+        forceDNAtolower = FALSE)
+      newname <- basename(j)
+      #only keep chromosome - add later as decision point??
+      genomes[[newname]] <- f[[1]]
+    }
+  }
 }
 
 seqinr::write.fasta(
   sequences = genomes,
   names = names(genomes),
-  file.out = paste0(output_dir, "consensus.subs.fasta")
-  )
+  file.out = paste0(output_dir, "/consensus.subs.fasta")
+)
