4a9ada19e9ec0205885df25d016dff71dfdbdc42
Tamas Stirling <stirling.tamas@gmail.com>
2022-06-28 22:59:50 +0200

    Add custom borders

--- R/neighbors.R
+++ R/neighbors.R
@@ -3,37 +3,31 @@
 #' This function uses an external database to decide whether two countries have
 #' land borders.
 #' 
-#' @param x character; country identifier, e.g. name
-#' @param y character; country identifier, e.g. name
-#' @param from character; identifier type, can be one of \code{"cctld"},
-#' \code{"country.name"}, \code{"country.name.de"}, \code{"country.name.fr"},
-#' \code{"country.name.it"}, \code{"cowc"}, \code{"cown"}, \code{"dhs"},
-#' \code{"ecb"}, \code{"eurostat"}, \code{"fao"}, \code{"fips"}, \code{"gaul"},
-#' \code{"genc2c"}, \code{"genc3c"}, \code{"genc3n"}, \code{"gwc"},
-#' \code{"gwn"}, \code{"imf"}, \code{"ioc"}, \code{"iso2c"}, \code{"iso3c"},
-#' \code{"iso3n"}, \code{"p4c"}, \code{"p4n"}, \code{"un"}, \code{"un_m49"},
-#' \code{"unicode.symbol"}, \code{"unhcr"}, \code{"unpd"}, \code{"vdem"},
-#' \code{"wb"}, \code{"wb_api2c"}, \code{"wb_api3c"}, \code{"wvs"},
-#' \code{"country.name.en.regex"}, \code{"country.name.de.regex"},
-#' \code{"country.name.fr.regex"} or \code{"country.name.it.regex"}
+#' @param x character; a vector of two characters ISO country codes 
+#' @param y character; a vector of two characters ISO country codes
+#' @param custom_borders an object of class \code{custom_borders} which
+#' modifies the default database.
+#' @note The database which is used by default does not include e.g. borders of
+#' countries that are not universally recognised. However, you can customise
+#' your database by adding or removing borders between countries. The
+#' \code{custom_borders} argument lets you specify such database.
 #' @references https://github.com/wmgeolab/rgeoboundaries
 #' @examples
 #' # Hungary and Slovakia have a land border 
-#' neighbors("hungary", "slovakia")
+#' neighbors("HU", "SK")
 #' 
 #' # Hungary and Greece do not have a land border
-#' neighbors("hungary", "greece")
+#' neighbors("HU", "GR")
 #' @export
-neighbors <- function(x, y, id.type = "country.name") {
+neighbors <- function(x, y, custom_borders = NULL) {
+  # TODO: too slow when many comparions required -> speed up
   data(country_borders, envir = rlang::current_env())
-  iso2c_x <- countrycode::countrycode(
-    x, origin = id.type, destination = "iso2c")
-  iso2c_y <- countrycode::countrycode(
-    y, origin = id.type, destination = "iso2c")
-  res <- ifelse(sum(
-    country_borders$iso2c1 %in% iso2c_x & country_borders$iso2c2 %in% iso2c_y),
-    TRUE,
-    FALSE
-  )
+  if (!is.null(custom_borders)) {
+    country_borders <- edit_borders(custom_borders)
+  }
+  res <- unname(mapply(function(a, b) {
+   res <-  sum(country_borders$iso2c1 %in% a & country_borders$iso2c2 %in% b)
+   res <- ifelse(res > 0, TRUE, FALSE)
+   }, x, y))
   return(res)
 }
