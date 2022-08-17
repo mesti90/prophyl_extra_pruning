adbeb20f3bfe1135a5fcc41616e060617e91b96c
Tamas Stirling <stirling.tamas@gmail.com>
2022-08-17 21:08:15 +0200

    Fix a bug around subsample filenames.

--- bin/subsample_input.R
+++ bin/subsample_input.R
@@ -6,12 +6,13 @@ nsubs <- as.numeric(args[2])
 ss <- round(nrow(assemblies)*as.numeric(args[3]), 0)
 
 set.seed(0)
-digits <- ceiling(log10(nsubs))
+digits <- ceiling(log10(nsubs+1))
 for (i in 1:nsubs) {
   zeroes <- digits - floor(log10(i))
+  filename = paste0(c("subsample_", rep(0, times = zeroes), i, ".tsv"), collapse = "")
   write.table(
     assemblies[sample(1:nrow(assemblies), ss, replace = FALSE), ],
-    file = paste0("subsample_", rep(0, times = zeroes), i, ".tsv"),
+    file = filename,
     sep = "\t",
     row.names = FALSE,
     quote = FALSE
